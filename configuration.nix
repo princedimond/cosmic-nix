b50e7a5322d4fb89f0919bb9ef2464fc4ffaa00b
princedimond <princedimond@gmail.com>
2025-04-05 00:12:57 -0400

    re-added expressvpn
    expressvpn package was fixed so re-added

--- configuration.nix
+++ configuration.nix
@@ -50,8 +50,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Enable the Cosmic Desktop Environment
   services.desktopManager.cosmic.enable = true;
@@ -147,7 +147,7 @@
     apacheHttpd
     nemo
     hplip
-    #expressvpn
+    expressvpn
     inputs.nixvim.packages.x86_64-linux.default
   ];
 
@@ -182,7 +182,7 @@
   programs.neovim.vimAlias = true;
 
   # List services that you want to enable:
-  #services.expressvpn.enable = true;
+  services.expressvpn.enable = true;
 
   # Enable Flakes
   nix.settings.experimental-features = [
