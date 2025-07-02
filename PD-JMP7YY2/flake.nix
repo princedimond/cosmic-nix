bdbbbca863595b223d61599c538cb21e52a9b675
princedimond <162772652+princedimond@users.noreply.github.com>
2025-07-02 11:17:28 -0400

    added home-manager and cosmic-manager
    added home manager and cosmic manager to the flake

--- PD-JMP7YY2/flake.nix
+++ PD-JMP7YY2/flake.nix
@@ -3,18 +3,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # NOTE: change "unstable" to "24.05" if you are using NixOS 24.05
     nixvim.url = "github:dc-tec/nixvim";
     zen-browser.url = "github:MarceColl/zen-browser-flake";
-   # nixos-cosmic = {
-    #  url = "github:lilyinstarlight/nixos-cosmic";
-     # inputs.nixpkgs.follows = "nixpkgs";
-    #};
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+     cosmic-manager = {
+      url = "github:HeitorAugustoLN/cosmic-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+     home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{
       self,
       nixpkgs,
-      #nixos-cosmic,
+      cosmic-manager,
+      home-manager,
       nixvim,
       zen-browser,
       nix-flatpak,
